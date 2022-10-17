44b06cf37dd399b09a0aaa47b87513591fda4594
roflseech <38049915+roflseech@users.noreply.github.com>
2022-10-17 20:19:02 +0300

    ObservableEvent.Raise

--- Assets/Scripts/ReactiveExtensions/ObservableEvent.cs
+++ Assets/Scripts/ReactiveExtensions/ObservableEvent.cs
@@ -6,6 +6,12 @@ namespace ReactiveAndEventBus
     public class ObservableEvent : IObservable<Unit>
     {
         private Subject<Unit> _subject;
+
+        public void Raise()
+        {
+            _subject.OnNext(Unit.Default);
+        }
+
         public IDisposable Subscribe(IObserver<Unit> observer)
         {
             return (_subject ?? (_subject = new Subject<Unit>())).Subscribe(observer);
